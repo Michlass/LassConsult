428e5e059581845ca1f2ac005197d6e7790a19e5
Michiel Lassche <michiel.lassche@gmail.com>
2016-12-30 22:00:43 +0100

    Removed doxygen documentation and added useful information
    Since doxygen requires a lot of modifications in order to properly
    create documentation, I have decided to remove this until some other
    time.
    
    The additional added documentation inculde data commands and information
    about a typical lamda-sensor (which I intend to read in order to
    determine fuel efficiency)

--- doxygen/html/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['installing_20console_20dash_20on_20raspberry_20pi',['Installing console dash on raspberry pi',['../md__c_1__users_michi__documents__github__lass_consult__r_e_a_d_m_e.html',1,'']]]
-];
